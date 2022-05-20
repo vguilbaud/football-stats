df984f3dad8e889117badd5b82a17cd539a61775
vguilbaud <vincentguilbaud@live.fr>
2022-05-20 15:30:53 +0200

    add leagues and coachs routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   //   .then((res) => {
   //     console.log(res);
   //   });
+
   // fetch("http://localhost:4200/users", {
   //   body: JSON.stringify({
   //     name: "rené",
@@ -28,6 +29,7 @@ function App() {
   //   .then((res) => {
   //     console.log(res);
   //   });
+
   // fetch("http://localhost:4200/users")
   //   .then((res) => {
   //     return res.json();
@@ -35,21 +37,66 @@ function App() {
   //   .then((res) => {
   //     console.log(res);
   //   });
+
   // fetch("http://localhost:4200/api/leagues/39?season=2021")
   //   .then((res) => {
   //     return res.json();
   //   })
   //   .then((res) => console.log(res));
+
   // fetch("http://localhost:4200/api/leagues?search=premie")
   //   .then((res) => {
   //     return res.json();
   //   })
   //   .then((res) => console.log(res));
-  fetch("http://localhost:4200/api/teams/33/statistics?season=2021")
+
+  // fetch(
+  //   "http://localhost:4200/api/leagues/getLeaguesFromTeam?team=33&season=2021"
+  // )
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then(async (res) => {
+  //     let allGoals = await res.map(async (daLeague) => {
+  //       const response = await fetch(
+  //         `http://localhost:4200/api/teams/33/statistics?season=2021&league=${daLeague.id}`
+  //       );
+  //       const resp = await response.json();
+  //       return {
+  //         matches: resp.matches,
+  //         victories: resp.victories,
+  //         draws: resp.draws,
+  //         loses: resp.loses,
+  //         goals: resp.goals,
+  //       };
+  //     });
+  //     return Promise.all(allGoals);
+  //   })
+  //   .then((res) => {
+  //     let total = { matches: 0, victories: 0, draws: 0, loses: 0, goals: 0 };
+  //     res.forEach((league) => {
+  //       total.matches += league.matches;
+  //       total.victories += league.victories;
+  //       total.draws += league.draws;
+  //       total.loses += league.loses;
+  //       total.goals += league.goals;
+  //     });
+  //     console.log(total);
+  //   });
+
+  // fetch("http://localhost:4200/api/teams/33/statistics?season=2021&league=39")
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((res) => console.log(res));
+
+  fetch("http://localhost:4200/api/coach?team=33")
     .then((res) => {
       return res.json();
     })
-    .then((res) => console.log(res));
+    .then((response) => {
+      console.log(response);
+    });
 
   return (
     <Layout className="centered">
